c1f73c07be27b344ceb82f13f12505fd6196cfcc
augxtoo <giovani.lepre1b1@gmail.com>
2024-03-07 08:45:01 -0300

    pasta titulo

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Title from './Title/';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Giovani</Text>
       <Text>Bruno</Text>
+      <Text>07-03-2024</Text>
+      <Title/>
       <StatusBar style="auto" />
     </View>
   );
